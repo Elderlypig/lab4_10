100f28b72e8cdc9fb997e122df439474064f7cd5
Elderlypig <egorprokuda742@gmail.com>
2024-05-13 15:52:40 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+let a = 5;
+let b = 2;
+let c = a-b;
+let d = 3;
+let result = c+d;
+alert(result);
+
+let str = "Привет, Мир!";
+alert(str);
+let str1 ="Привет, ";
+let str2 = "Мир!";
+alert(str1+str2);
+let name = "Павел Олегович";
+alert("Здраствуйте, " + name);
+let age = 18;
+alert("Мне " + age + " лет")
+
+let name1 = prompt("Как вас зовут?");
+alert("Привет, " + name1);
+
+let num = prompt("Напишите число");
+alert("Квадрат этого числа равен " + num*num);
